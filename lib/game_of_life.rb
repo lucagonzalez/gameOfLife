b87bbfc371a4b16ab7bd198d678ac601b90e0c9f
Luca Clemente <luca.gonzalezp@gmail.com>
2019-09-16 17:53:32 +0200

    Kata finished

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -1,3 +1,42 @@
 def game_of_life(grid)
-  grid.gsub(/\*/, ".")
+  final_string = ""
+  grid_as_array = grid.split("")
+  adjacent_position_grid = grid_as_array[2].to_i + 1
+  grid_as_array.each.with_index { |character, index|
+    if index > 3 && character != "\n"
+      neighbours_count = count_neighbours(grid_as_array, index, adjacent_position_grid)
+      if edge?(grid, grid_as_array, index, adjacent_position_grid) || neighbours_count < 2 || neighbours_count >= 4
+        final_string += "."
+      elsif neighbours_count == 3
+        final_string += "*"
+      else
+        final_string += character
+      end
+    else
+      final_string += character
+    end}
+  final_string
+end
+
+def edge?(grid, grid_as_array, index, adjacent_position_grid)
+  if (index - adjacent_position_grid) < 0 || (index + adjacent_position_grid) > grid.size #Verifies cells on the edge first and last rolls
+    true
+  elsif grid_as_array[index - 1] == "\n" || grid_as_array[index + 1] == "\n" || index + 1 > grid.size #Verifies cells on the edge columns
+    true
+  else
+    false
+  end
+end
+
+def count_neighbours(grid_as_array, index, adjacent_position_grid)
+  vecino = 0
+  vecino += 1 if grid_as_array[index + 1] == "*"
+  vecino += 1 if grid_as_array[index - 1] == "*"
+  vecino += 1 if grid_as_array[index - adjacent_position_grid] == "*"
+  vecino += 1 if grid_as_array[index + adjacent_position_grid] == "*"
+  vecino += 1 if grid_as_array[index + adjacent_position_grid + 1] == "*"
+  vecino += 1 if grid_as_array[index + adjacent_position_grid - 1] == "*"
+  vecino += 1 if grid_as_array[index - adjacent_position_grid + 1] == "*"
+  vecino += 1 if grid_as_array[index - adjacent_position_grid - 1] == "*"
+  vecino
 end
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -23,10 +23,46 @@ describe 'Game of Life' do
 
     expect(game_of_life(one_cell_grid)).to eq no_cell_grid
   end
-  it "is given two living cells in any position" do
-    one_cell_grid = "4 8\n........\n....**..\n........\n........"
+  it "is given one living cell in the edge" do
+    one_cell_grid = "4 8\n*.......\n........\n........\n........"
     no_cell_grid = "4 8\n........\n........\n........\n........"
 
     expect(game_of_life(one_cell_grid)).to eq no_cell_grid
+  end
+  it "is given two neighbours live cells in the same roll" do
+    two_neighbours_cell_grid = "4 8\n........\n..***...\n........\n........"
+    two_cell_grid = "4 8\n........\n...*....\n...*....\n........"
+
+    expect(game_of_life(two_neighbours_cell_grid)).to eq two_cell_grid
+  end
+  it "is given three neighbours live cells in the same column" do
+    two_neighbours_cell_grid = "4 8\n...*....\n...*....\n...*....\n........"
+    three_cell_grid = "4 8\n........\n..***...\n........\n........"
+
+    expect(game_of_life(two_neighbours_cell_grid)).to eq three_cell_grid
+  end
+  it "is given two groups of three neighbours live cells in the same roll, on the edge" do
+    two_neighbours_cell_grid = "4 8\n..***...\n........\n........\n.***...."
+    two_cell_grid = "4 8\n........\n...*....\n..*.....\n........"
+
+    expect(game_of_life(two_neighbours_cell_grid)).to eq two_cell_grid
+  end
+  it "is given two groups of three neighbours live cells in the same column, on the edge" do
+    two_neighbours_cell_grid = "4 8\n*......*\n*......*\n*......*\n........"
+    two_cell_grid = "4 8\n........\n.*....*.\n........\n........"
+
+    expect(game_of_life(two_neighbours_cell_grid)).to eq two_cell_grid
+  end
+  it "is given four neighbours live cells, one beeing on the lower roll" do
+    three_neighbours_cell_grid = "4 8\n........\n..***...\n...*....\n........"
+    six_cell_grid = "4 8\n........\n..***...\n..***...\n........"
+
+    expect(game_of_life(three_neighbours_cell_grid)).to eq six_cell_grid
+  end
+  it "is given four neighbours live cells, one beeing on the upper roll" do
+    three_neighbours_cell_grid = "4 8\n........\n...*....\n..***...\n........"
+    six_cell_grid = "4 8\n........\n..***...\n..***...\n........"
+
+    expect(game_of_life(three_neighbours_cell_grid)).to eq six_cell_grid
   end
 end
